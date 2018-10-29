0c47e343ecdc957a869756add264a37b4d5d2886
Jay Soni <jaysoni@engineer.com>
2018-10-29 13:10:57 +0530

    Simple Type Script

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+exports.__esModule = true;
+var msg = 'sdljfsdf';
+var x = 10;
+var flag = true;
+var name = 'jay soni';
+var sentance = "you " + name;
+var n = null;
+var u = undefined;
+var list1 = [1, 2, 3];
+var touple = ['st', 'sdlfj'];
+var Color;
+(function (Color) {
+    Color[Color["a"] = 34] = "a";
+    Color[Color["b"] = 24] = "b";
+})(Color || (Color = {}));
+;
+var randomValue = 'sdlfJ3';
+// randomValue = 23
+// console.log((randomValue as string).toLowerCase());
---
+++ main.ts
@@ -0,0 +1,20 @@
+export {}
+let msg = 'sdljfsdf';
+
+let x:number = 10;
+let flag:boolean = true;
+let name:string = 'jay soni';
+let sentance:string = `you ${name}`;
+
+let n: null = null;
+let u: undefined = undefined;
+
+let list1: number[] = [1,2,3];
+
+let touple: string[]  = ['st','sdlfj'];
+
+enum Color{a = 34,b = 24};
+
+let randomValue: unknown = 23;
+
+// console.log((randomValue as string).toLowerCase());
